a643cc6a41a6450e1a80e506a755911dfc2cb77b
Safouane El-Amine BENELKADI <s-amine.benelkadi@etu.uca.fr>
2022-01-21 03:16:35 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:17-jdk-alpine3.14
+MAINTAINER safouane.benelkadi
+COPY target/backend-0.0.1-SNAPSHOT.jar backend-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","/backend-0.0.1-SNAPSHOT.jar"]
---
+++ src/test/kotlin/com/inventorymanager/g5/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.inventorymanager.g5.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
