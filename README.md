cbe10e886fe6575fe42f546453d534f049f602f3
ouboumour <oussamaboumour7@gmail.com>
2022-02-06 18:45:35 +0100

    #9: Add swagger + swagger-ui dependencies :heavy_plus_sign:

---
+++ README.md
@@ -0,0 +1,2 @@
+## How to access to swagger-ui gui ?
+Click the link [here](http://localhost:8080/swagger-ui/index.html)
--- src/main/kotlin/com/inventorymanager/g5/backend/BackendApplication.kt
+++ src/main/kotlin/com/inventorymanager/g5/backend/BackendApplication.kt
@@ -2,8 +2,12 @@ package com.inventorymanager.g5.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
 class BackendApplication
 
 fun main(args: Array<String>) {
